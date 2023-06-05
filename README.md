8dbae5ede871b10c5a83cb3fcc058cca3e9ee09a
Haoxi Tan <h4sh5@pm.me>
2023-06-05 23:40:09 +1000

    scanner scripts and github action

---
+++ README.md
@@ -0,0 +1,5 @@
+# pypi auto scanner
+
+A github action that fetches the latest pypi packages and scans them. Report any issues found in issues.
+
+
---
+++ get_packages.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+'''
+a script to pull new pypi packages and print out all their names
+'''
+
+import xml.etree.ElementTree as ET
+from urllib.request import urlopen
+import os.path
+
+# xml is crap, pypi please change to json at some point
+url = 'https://pypi.org/rss/updates.xml'
+
+response = urlopen(url)
+body = response.read()
+if response.status != 200:
+    print("ERROR: response:", response.status, body)
+
+# read already scanned list
+already_scanned = []
+if os.path.isfile('scanned_pkgs.txt'):
+    already_scanned = open('scanned_pkgs.txt','r').read().split('\n').strip()
+
+root = ET.fromstring(body)
+
+for chan in root[0]:
+    for e in chan:
+        if e.tag == 'link':
+            link = e.text
+            name = link.split('/')[4]
+            if name not in already_scanned:
+                print(name)
+
+# debugging
+#import code
+#code.interact(local=locals())
+
+
