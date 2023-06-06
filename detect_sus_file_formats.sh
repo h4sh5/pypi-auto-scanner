83051f1bf636e86f05fb1e0f6e220a25f8faa549
Haoxi Tan <h4sh5@pm.me>
2023-06-06 19:36:31 +1000

    efficiency improvement

--- detect_sus_file_formats.sh
+++ detect_sus_file_formats.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-find packages | parallel file | grep executable | grep -v text | tee -a sus_files.txt
-find packages | parallel file | grep -F .pyc | tee -a sus_files.txt
-find packages | parallel file | grep -F .exe | tee -a  sus_files.txt
-find packages | parallel file | grep -F .sh | tee -a sus_files.txt
-
+find packages | parallel file | grep -E '(executable)|(\.exe)|(\.pyc)' | grep -v text | tee -a sus_files.txt
 cp sus_files.txt sus_files.txt.1
 sort -u sus_files.txt.1 > sus_files.txt
 rm sus_files.txt.1
