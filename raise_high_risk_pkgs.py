585f319e506cd7815cd5c6e0828450b9807a7e50
Haoxi Tan <h4sh5@pm.me>
2023-06-06 17:55:38 +1000

    raising issues and better output

--- raise_high_risk_pkgs.py
+++ raise_high_risk_pkgs.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import json
+import os
 import sys
+from urllib.request import urlopen,Request
 
 '''
 script to parse semgrep json output
 '''
+
+def create_github_issue(body):
+    url = 'https://api.github.com/repos/h4sh5/pypi-auto-scanner/issues'
+    request = Request(url, data=body, headers={"Accept":"application/vnd.github+json", "Authorization":f"Bearer {os.getenv('GITHUB_TOKEN')}", "X-GitHub-Api-Version":"2022-11-28"})
+    r = urlopen(request)
+    data = r.read()
+    if r.status != 200:
+        print('ERROR create_github_issue', r.status, data)
+
+    
 
 file = 'report.json'
 
@@ -35,9 +47,12 @@ for name in pkg_detections:
         if severity == "ERROR":
             error_exists = True
             break
-    if len(pkg_detections[name]) > 2 and error_exists:
+    if error_exists or len(pkg_detections[name]) > 3:
         print("High risk pkg:", name)
-        with open('high_risk_pkgs.txt','a+') as f:
-            f.write(name+'\n')
+        with open('high_risk_pkgs.csv','a+') as f:
+            f.write(f'{name},{len(pkg_detections[name])}\n')
+        issue_data = {"title":f"{name} has {len(pkg_detections[name])} detections", "body":json.dumps(pkg_detections[name],indent=2), "labels":["suspicious"]}
+        create_github_issue(issue_data)
+
 #import code
 #code.interact(local=locals())
