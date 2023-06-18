f4f2df3c49cb8f6c635a9689e113c99ccea5b0ef
Haoxi Tan <haoxi.tan@gmail.com>
2023-06-18 10:54:36 +1000

    no more git based dedup

--- detect_sus_file_formats.sh
+++ detect_sus_file_formats.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 find packages | parallel file | grep -E '(executable)|(\.exe)|(\.pyc)' | grep -v text | tee new_sus_files.txt
-cat new_sus_files.txt >> sus_files.txt
-# dedup sus_files.txt
-cp sus_files.txt sus_files.txt.1
-sort -u sus_files.txt.1 > sus_files.txt
-rm sus_files.txt.1
-
